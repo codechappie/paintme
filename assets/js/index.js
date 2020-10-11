0beeebd3d7715528fe795a21b03b92d4469219ff
codechappie <davidproyecto01@gmail.com>
2020-10-11 18:38:50 -0500

    version estable v0.0.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,22 @@
 const btnPaintme = document.getElementById("paintme-button");
 const paintme = document.getElementById("paintme");
 
+const btnCloseModal = document.querySelectorAll(".btn-close-modal-size");
+
+const btnSize = document.getElementById("btn-size");
+const modalSize = document.getElementById("size-modal");
+
+
+btnCloseModal.forEach(buttonCloseModal => {
+    buttonCloseModal.addEventListener("click", ()=>{
+        modalSize.classList.toggle("show");
+    });
+});
 
 btnPaintme.addEventListener("click", ()=>{
     paintme.classList.toggle("show");
+});
+
+btnSize.addEventListener("click", ()=>{
+    modalSize.classList.toggle("show");
 });
